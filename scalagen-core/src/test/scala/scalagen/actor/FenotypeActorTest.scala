4d03685e32ec62913360f8b237f589fd49c4e76c
Przemysław Piotrowski <piotrowski.przemek@gmail.com>
2014-05-03 16:18:13 +0200

    Tests asynchronous -> synchronous
    Added trait to stop actor system after the test

--- scalagen-core/src/test/scala/scalagen/actor/FenotypeActorTest.scala
+++ scalagen-core/src/test/scala/scalagen/actor/FenotypeActorTest.scala
@@ -1,25 +1,20 @@
 package scalagen.actor
 
-import akka.actor.{Props, ActorSystem}
-import akka.testkit.{ImplicitSender, TestKit}
-import org.scalatest.{WordSpecLike, ShouldMatchers, BeforeAndAfterAll}
-import scalagen.actor.Fenotype
+import akka.actor.ActorSystem
+import akka.testkit.{TestActorRef, ImplicitSender, TestKit}
+import org.scalatest.WordSpecLike
 import scalagen.message.ReadGenom
+import utils.StopSystemAfterAll
 
-class FenotypeActorTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
-  with ShouldMatchers with WordSpecLike with BeforeAndAfterAll {
-
-  def this() = this(ActorSystem("FenotypeTestActorSystem"))
-
-  val sampleGenotype = List[Int](1, 4, 12, 3)
-  val fenotypeActorRef = system.actorOf(Props(classOf[Fenotype[List[Int]]], sampleGenotype))
-
-  override def afterAll() {
-    TestKit.shutdownActorSystem(system)
-  }
+class FenotypeActorTest extends TestKit(ActorSystem("FenotypeTestActorSystem"))
+with ImplicitSender
+with WordSpecLike
+with StopSystemAfterAll {
 
   "An FenotypeActor" must {
     "respond with its genotype" in {
+      val sampleGenotype = List[Int](1, 4, 12, 3)
+      val fenotypeActorRef = TestActorRef(new Fenotype(sampleGenotype))
       fenotypeActorRef ! ReadGenom
       expectMsg(sampleGenotype)
     }
