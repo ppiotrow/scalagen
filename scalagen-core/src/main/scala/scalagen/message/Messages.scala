682494b31c44146bc07807bb6acae90cfceb8811
Tomasz Rozbicki <tomrozb+code@gmail.com>
2014-05-07 16:32:09 +0200

    Procreator actor implementation

--- scalagen-core/src/main/scala/scalagen/message/Messages.scala
+++ scalagen-core/src/main/scala/scalagen/message/Messages.scala
@@ -8,3 +8,7 @@ case object ReadGenom
 case class Eval(fenotype: ActorRef, genotype: Genome)
 
 case class Evaluated(fenotype: ActorRef, value: Double)
+
+case class Reproduce(genomeA: Genome, genomeB: Genome)
+
+case class Descendant(genome: Genome)
